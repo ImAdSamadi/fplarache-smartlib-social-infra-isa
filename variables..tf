94d67c1e7349c65ce64a85a9f000c03900f9ea20
ImAdSamadi <150534956+ImAdSamadi@users.noreply.github.com>
2024-11-25 22:18:14 +0100

    Update variables..tf

--- variables..tf
+++ variables..tf
@@ -1,21 +1,21 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "eu-north-1"
 }
 
 variable "ecr_repository_name" {
-  default = "my-app-repo"
+  default = "my-app-repo-imad"
 }
 
 variable "ecs_cluster_name" {
-  default = "my-app-cluster"
+  default = "my-app-cluster-imad"
 }
 
 variable "ecs_task_family" {
-  default = "my-app-task-family"
+  default = "my-app-task-family-imad"
 }
 
 variable "ecs_service_name" {
-  default = "my-app-service"
+  default = "my-app-service-imad"
 }
 
 variable "subnet_ids" {
